95a6a894ca2ef4a2f0fd8a86545b2c1a3c9df697
liahychka <157300297+liahychka@users.noreply.github.com>
2024-07-05 16:33:20 +0200

    .

---
+++ src/js/header-modal.js
@@ -0,0 +1,27 @@
+// import Accordion from 'accordion-js';
+
+// const container = document.querySelector('.name-list-menu');
+// const accordion1 = new Accordion(container, {
+//     showMultiple: true,
+//     onOpen: (currEl) => console.log('Open!', currEl)
+// });
+ 
+// const buttons = document.querySelectorAll('.mobile-close-btn');
+//     buttons.forEach((button, idx) => {
+//         button.addEventListener('click', () => accordion1.close(idx));
+//     });
+
+// const markup = ulTemplate();
+// container.innerHTML = markup;
+
+
+// function ulTemplate(ul) {
+//     return ul.map(liTemplate).join('');
+// }
+
+// function liTemplate(li) {
+//     return `<li class="item-navigation-menu"><a href="#aboutme">About me</a></li>
+//         <li class="item-navigation-menu"><a href="#benefits">Benefits</a></li>
+//         <li class="item-navigation-menu"><a href="#projects">Projects</a></li>
+//         <li class="item-navigation-menu"><a href="#faq">FAQ</a></li>`;
+// }
