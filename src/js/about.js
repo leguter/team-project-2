99489c6a97b23ebafc761719ad2f961f0a6b7458
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-07-06 19:15:51 +0300

    make about-js

--- src/js/about.js
+++ src/js/about.js
@@ -10,32 +10,44 @@ const button2 = document.querySelector('#ac-trigger-1');
 const button3 = document.querySelector('#ac-trigger-2');
 
 
-const isOpen1 = button1.getAttribute('aria-expanded');
-const isOpen2 = button2.getAttribute('aria-expanded');
-const isOpen3 = button3.getAttribute('aria-expanded');
-
+let isOpen1 = button1.getAttribute('aria-expanded');
+let isOpen2 = button2.getAttribute('aria-expanded');
+let isOpen3 = button3.getAttribute('aria-expanded');
 
 let svgUp = document.querySelectorAll('.icon-up');
 let svgDown = document.querySelectorAll('.icon-down');
 
-// function toggle (isOpen1, isOpen2, isOpen3) {
-//   if (isOpen1) {
-//     svgUp = svgUp[0].classList.add('is-open');
-//   } else {
-//     svgDown = svgDown[0].classList.add('is-open');
-//   };
+console.log(svgUp);
+
+function addClassOpen1 (isOpen1) {
+  if (isOpen1) {
+    svgUp = svgUp[0].classList.add('is-open');
+  } else {
+    svgDown = svgDown[0].classList.add('is-open');
+  };
+
+};
+
+
+addClassOpen1(isOpen1);
+addClassOpen2(isOpen2);
+addClassOpen3(isOpen3);
 
 
 // };
 
 // toggle(isOpen1, isOpen2, isOpen3);
-button1.addEventListener('click', toggleMenu(isOpen1));
-button2.addEventListener('click', toggleMenu(isOpen2));
-button3.addEventListener('click', toggleMenu(isOpen3));
+
+// function toggleMenu(value) {
+//   console.log(value);
+// };
 
 
-function toggleMenu(value) {
-  console.log(value);
-};
+// button1.addEventListener('click', toggleMenu(isOpen1));
+// button2.addEventListener('click', toggleMenu(isOpen2));
+// button3.addEventListener('click', toggleMenu(isOpen3));
+
+
+
 
 
