8881047f6809f51f8cedd9d187e282869b801c4e
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-07-08 11:09:07 +0300

    make section

--- src/js/about.js
+++ src/js/about.js
@@ -4,30 +4,17 @@ new Accord('.accord-container', {
   openOnInit: [0],
 });
 
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
+import Swiper from '../../node_modules/swiper/swiper-bundle.min.mjs';
 
-new Swiper('.slider', {
+new Swiper('.swiper-container', {
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.button-next',
   },
+  loop: true,
+  spaseBetween: 1,
 });
 
 
-
-// const button1 = document.querySelector('#ac-trigger-0');
-// const button2 = document.querySelector('#ac-trigger-1');
-// const button3 = document.querySelector('#ac-trigger-2');
-
-
-// let isOpen1 = button1.getAttribute('aria-expanded');
-// let isOpen2 = button2.getAttribute('aria-expanded');
-// let isOpen3 = button3.getAttribute('aria-expanded');
-
-// let svgUp = document.querySelectorAll('.icon-up');
-// let svgDown = document.querySelectorAll('.icon-down');
 
 
 
