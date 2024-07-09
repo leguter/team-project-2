9cf0e453297fb1d9503a1995757d3e85af3161dc
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-07-09 23:07:40 +0300

    projects-slider

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,6 +1,6 @@
 import Swiper from '../../node_modules/swiper/swiper-bundle.min.mjs';
 import 'swiper/css';
-new Swiper('.swiper-my', {
+new Swiper('.swiper-container', {
 navigation: {
     nextEl: '.swiper-button-prev',
     prevEl: '.swiper-button-next',
