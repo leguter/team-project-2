480c2e815127cea773a4678d8ab805a6dfdc5364
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-07-10 10:09:05 +0300

    make new slider

--- src/js/about.js
+++ src/js/about.js
@@ -1,15 +1,13 @@
 import Accord from 'accordion-js';
-import { Navigation } from 'swiper/modules';
 new Accord('.accord-container', {
   openOnInit: [0],
 });
 
-import Swiper from '../../node_modules/swiper/swiper-bundle.min.mjs';
-import 'swiper/css';
-new Swiper('.swiper-about', {
-  // modules: [Navigation],
+import Swiper from 'swiper';
+new Swiper('.swiper-container', {
+
   navigation: {
-    nextEl: '.swiper-button-next',
+    nextEl: 'button-next',
   },
   loop: true,
   slidesPerView: 2,
